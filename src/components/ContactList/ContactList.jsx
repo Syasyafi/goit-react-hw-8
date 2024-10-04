5d9f7b5b1c2bee0610cf104b233a255715439e6b
Nastya <nastya.syasyafi@gmail.com>
2024-10-04 22:13:33 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,6 +4,15 @@ import { useSelector } from "react-redux";
 import { selectFilteredContacts } from "../../redux/contacts/selectors";
 
 export default function ContactList() {
+  // const contacts = useSelector(selectContacts);
+  // const filters = useSelector(selectFilters);
+
+  // const allUsersWithFiltred =
+  //   contacts &&
+  //   contacts.filter((value) =>
+  //     value.name.toLocaleLowerCase().includes(filters.toLocaleLowerCase())
+  //   );
+
   const allUsersWithFiltred = useSelector(selectFilteredContacts);
 
   return (
